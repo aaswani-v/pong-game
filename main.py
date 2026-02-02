d875aff922bb8b3583a164dcd76911b68830f7d5
aaswani-v <avr9766@gmail.com>
2026-02-02 22:52:27 +0530

    basic structure which creates a blank window

--- main.py
+++ main.py
@@ -23,6 +23,34 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+            
+            pygame.draw.rect(screen, COLOR_WHITE, paddle_1_rect)
+            pygame.draw.rect(screen, COLOR_WHITE, paddle_2_rect)
+
+            pygame.draw.rect(screen, COLOR_WHITE, ball_rect)
+
+            pygame.display.update()
+
+paddle_1_rect = pygame.Rect(30, 0, 7, 100)
+paddle_2_rect = pygame.Rect(SCREEN_WIDTH - 50, 0, 7, 100)
+
+# tracking the paddle movement
+paddle_1_move = 0
+paddle_2_move = 0
+
+# rectangle representing the ball
+ball_rect = pygame.Rect(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2, 25, 25)
+
+ball_accel_x = random.randint(2,4) * 0.1
+ball_accel_y = random.randint(2,4) * 0.1
+
+#randomizing the initial direction of the ball
+if random.randint(1,2) * 1:
+    ball_accel_x *= -1
+if random.randint(1,2) * 1:
+    ball_accel_y *= -1
+
+
 
 if __name__ == "__main__":
     main()
