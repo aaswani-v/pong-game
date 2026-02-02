2fb2adf2858dec1a0d028df139dcc9a43bd848f5
aaswani-v <avr9766@gmail.com>
2026-02-02 22:08:11 +0530

    Starting this project

---
+++ main.py
@@ -0,0 +1,17 @@
+#importing the libraries
+import pygame
+import random
+
+#WINDOWS size
+SCREEN_WIDTH = 960
+SCREEN_HEIGHT = 720
+
+COLOR_BLACK = (0, 0, 0)
+COLOR_WHITE = (255, 255, 255)
+
+def main():
+    #GAME SETUP
+
+if __name__ == "__main__":
+    main()
+
