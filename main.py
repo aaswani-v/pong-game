1c001f2a2026cf82302aff4aba888cebdd372fb3
aaswani-v <avr9766@gmail.com>
2026-02-02 23:49:16 +0530

    everything is fine only need to add movemetn and logic

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ COLOR_BLACK = (0, 0, 0)
 COLOR_WHITE = (255, 255, 255)
 
 def main():
-    #GAME SETUP
+    global started  
 
     pygame.init()
 
@@ -18,40 +18,48 @@ def main():
     pygame.display.set_caption("PONG GAME")
 
     while True:
-        screen.fill(COLOR_BLACK) #FILL THE SCREEN WITH BLACK COLOR
+        clock.tick(60)  
+        screen.fill(COLOR_BLACK)
 
-        for event in pygame.event.get():
+        for event in pygame.event.get(): 
             if event.type == pygame.QUIT:
+                pygame.quit()
                 return
-            
-            pygame.draw.rect(screen, COLOR_WHITE, paddle_1_rect)
-            pygame.draw.rect(screen, COLOR_WHITE, paddle_2_rect)
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_SPACE:
+                    started = True
 
-            pygame.draw.rect(screen, COLOR_WHITE, ball_rect)
+        if not started:
+            font = pygame.font.Font(None, 30)
+            text = font.render("Press SPACE to start the game", True, COLOR_WHITE)
+            text_rect = text.get_rect(center=(SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2))
+            screen.blit(text, text_rect)
 
-            pygame.display.update()
+        pygame.draw.rect(screen, COLOR_WHITE, paddle_1_rect)
+        pygame.draw.rect(screen, COLOR_WHITE, paddle_2_rect)
+        pygame.draw.rect(screen, COLOR_WHITE, ball_rect)
 
+        pygame.display.update()
+
+
+# rectangle representing the paddles
 paddle_1_rect = pygame.Rect(30, 0, 7, 100)
 paddle_2_rect = pygame.Rect(SCREEN_WIDTH - 50, 0, 7, 100)
 
-# tracking the paddle movement
-paddle_1_move = 0
-paddle_2_move = 0
-
 # rectangle representing the ball
-ball_rect = pygame.Rect(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2, 25, 25)
+ball_rect = pygame.Rect(SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2, 25, 25)
 
 ball_accel_x = random.randint(2,4) * 0.1
 ball_accel_y = random.randint(2,4) * 0.1
 
+clock = pygame.time.Clock()
+started = False
+
 #randomizing the initial direction of the ball
-if random.randint(1,2) * 1:
+if random.randint(1,2):
     ball_accel_x *= -1
-if random.randint(1,2) * 1:
+if random.randint(1,2):
     ball_accel_y *= -1
-
-
 
 if __name__ == "__main__":
     main()
-
