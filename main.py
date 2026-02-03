d97b85bd0d76adc5ddb21d5574552168071d9240
aaswani-v <avr9766@gmail.com>
2026-02-04 00:17:54 +0530

    "[working] - 1. windows is comming >> >> 2. right side of the paddle is moving (using w,a,sd) >> >> [problem] -- >> >> 1. ball is not moving yet >> >> 2. right side of the paddle is stuck on the top like it is not comming down it can move up down but at the top of the screen only"

--- main.py
+++ main.py
@@ -9,14 +9,15 @@ SCREEN_HEIGHT = 720
 COLOR_BLACK = (0, 0, 0)
 COLOR_WHITE = (255, 255, 255)
 
+# Global variables
+started = False
+paddle_1_speed = 0
+paddle_2_speed = 0
+PADDLE_SPEED = 5
 
 # rectangle representing the paddles
 paddle_1_rect = pygame.Rect(30, 0, 7, 100)
 paddle_2_rect = pygame.Rect(SCREEN_WIDTH - 50, 0, 7, 100)
-
-paddle_1_speed = 0
-paddle_2_speed = 0
-PADDLE_SPEED = 0
 
 # rectangle representing the ball
 ball_rect = pygame.Rect(SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2, 25, 25)
@@ -25,7 +26,6 @@ ball_accel_x = random.randint(2,4) * 0.1
 ball_accel_y = random.randint(2,4) * 0.1
 
 clock = pygame.time.Clock()
-started = False
 
 #randomizing the initial direction of the ball
 if random.randint(1,2):
@@ -36,7 +36,7 @@ if random.randint(1,2):
 #----------------------------------------------------------#
 
 def main():
-    global started  
+    global started, paddle_1_speed, paddle_2_speed
 
     pygame.init()
 
@@ -51,18 +51,31 @@ def main():
             if event.type == pygame.QUIT:
                 pygame.quit()
                 return
+            
+            # for key down events
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_SPACE:
                     started = True
 
                 if event.key == pygame.K_w:
-        paddle_1_speed = -PADDLE_SPEED
-    if event.key == pygame.K_s:
-        paddle_1_speed = PADDLE_SPEED
-    if event.key == pygame.K_UP:
-        paddle_2_speed = -PADDLE_SPEED
-    if event.key == pygame.K_DOWN:
-        paddle_2_speed = PADDLE_SPEED
+                    paddle_1_speed = -PADDLE_SPEED
+                if event.key == pygame.K_s:
+                    paddle_1_speed = PADDLE_SPEED
+                if event.key == pygame.K_UP:
+                    paddle_2_speed = -PADDLE_SPEED
+                if event.key == pygame.K_DOWN:
+                    paddle_2_speed = PADDLE_SPEED
+                
+            # for key up events
+            if event.type == pygame.KEYUP:
+                if event.key in (pygame.K_w, pygame.K_s):
+                    paddle_1_speed = 0
+                if event.key in  (pygame.K_UP, pygame.K_DOWN):
+                    paddle_2_speed = 0
+                    
+                    
+            paddle_1_rect.y += paddle_1_speed
+            paddle_2_rect.y += paddle_2_speed
 
         if not started:
             font = pygame.font.Font(None, 30)
